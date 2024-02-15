fec1fe069d844412559292f126572d8426951f71
siimongmz <118911662+siimongmz@users.noreply.github.com>
2024-02-15 13:41:34 +0100

    Eliminado constructor y propiedad innecesarios

--- src/main/java/com/util/Usuario.java
+++ src/main/java/com/util/Usuario.java
@@ -8,15 +8,9 @@ import java.io.Serializable;
 public class Usuario implements Serializable {
     @Getter
     private final String nombreUsuario;
-    @Getter @Setter
-    private int puerto;
 
     public Usuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
     }
 
-    public Usuario(String nombreUsuario, int puerto) {
-        this.nombreUsuario = nombreUsuario;
-        this.puerto = puerto;
-    }
 }
